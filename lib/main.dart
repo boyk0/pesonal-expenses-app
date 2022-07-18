dadf103b3a2640421a9362473bc75c74f8dbcd6f
boyk0 <v_boiko-itssv@kw.com>
2022-07-18 14:21:54 +0300

    set theme

--- lib/main.dart
+++ lib/main.dart
@@ -6,18 +6,20 @@ import './widgets/new_transaction.dart';
 import './widgets/transaction_list.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.purple,
+        accentColor: Colors.amber,
+        // appBarTheme: AppBarTheme(
+        //   color: Colors.deepOrangeAccent,
+        // )
       ),
       home: MyHomePage(),
     );
